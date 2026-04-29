fd7de40374a3d80b7ecf8914c0c815baa439d5bc
Mala A N <130269273+mala27an@users.noreply.github.com>
2026-04-29 11:42:25 +0530

    Time: 8 ms (91.19%) | Memory: 49.4 MB (5.87%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,32 +3,31 @@ import java.util.*;
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        //Sort based on start value
+        //Sort based on first element
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
         //Create list to store merged intervals
-        List<int[]> list = new ArrayList<>();
+        List<int[]> l = new ArrayList<>();
 
         //Take first interval
-        int[] current = intervals[0];
-        list.add(current);
+        int[] present = intervals[0];
+        l.add(present);
 
         //Compare all remaining intervals
         for (int i = 1; i < intervals.length; i++) {
             int[] next = intervals[i];
-
             // If overlap
-            if (next[0] <= current[1]) {
-                current[1] = Math.max(current[1], next[1]);
+            if (next[0] <= present[1]) {
+                present[1] = Math.max(present[1], next[1]);
             } 
             // If no overlap
             else {
-                current = next;
-                list.add(current);
+                present = next;
+                l.add(present);
             }
         }
 
         //Convert list to array
-        return list.toArray(new int[list.size()][]);
+        return l.toArray(new int[l.size()][]);
     }
 }
