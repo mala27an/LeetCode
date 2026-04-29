7b1b123f05e1ca917c8bc67e253420f073683fb9
Mala A N <130269273+mala27an@users.noreply.github.com>
2026-04-29 11:20:55 +0530

    Time: 7 ms (98.83%) | Memory: 48.4 MB (99.01%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        // Step 1: Sort based on start value
+        //Sort based on start value
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        // Step 2: Create list to store merged intervals
+        //Create list to store merged intervals
         List<int[]> list = new ArrayList<>();
 
-        // Step 3: Take first interval
+        //Take first interval
         int[] current = intervals[0];
         list.add(current);
 
-        // Step 4: Compare all remaining intervals
+        //Compare all remaining intervals
         for (int i = 1; i < intervals.length; i++) {
             int[] next = intervals[i];
 
@@ -28,7 +28,7 @@ class Solution {
             }
         }
 
-        // Step 5: Convert list to array
+        //Convert list to array
         return list.toArray(new int[list.size()][]);
     }
 }
